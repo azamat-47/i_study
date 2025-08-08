8aa1fd6ee91edb284dc48225177c1e793b8a8433
az1kkRich <az1kkrichbe@gmail.com>
2025-08-08 18:08:21 +0500

    navbar and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Outlet, Link, useLocation } from "react-router-dom";
-import { Button, Layout, Menu, theme } from 'antd';
+import { Avatar, Button, Input, Layout, Menu, Popconfirm, theme, Tooltip } from 'antd';
 import { AiFillHome, AiFillSetting } from 'react-icons/ai';
 import { PiStudentFill } from 'react-icons/pi';
-import { MdGroups3 } from 'react-icons/md';
+import { MdGroups3, MdPayment } from 'react-icons/md';
 import { RiMenuFold2Fill, RiMenuUnfold2Fill } from 'react-icons/ri';
+import { FaUserNurse } from 'react-icons/fa';
+import { CiSquareChevLeft } from 'react-icons/ci';
+const { Search } = Input;
 
 const { Header, Sider, Content } = Layout;
 
@@ -16,10 +19,16 @@ const App = () => {
 
   const location = useLocation();
 
+  const handleConfirm = () => {
+    console.log('Confirmed exit');
+    // Here you can handle navigation, logout, etc.
+  };
+
+
   return (
-    <Layout >
+    <Layout className='min-h-[100vh]!' >
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical "  />
+        <div className="demo-logo-vertical " />
         <Menu
           theme="dark"
           mode="inline"
@@ -27,22 +36,22 @@ const App = () => {
           items={[
             {
               key: '/',
-              icon: <AiFillHome />,
-              label: <Link to="/">Asosiy</Link>,
+              icon: <MdGroups3 size={20} />,
+              label: <Link to="/">Guruhlar</Link>,
             },
             {
               key: '/payment',
-              icon: <MdGroups3 />,
+              icon: <MdPayment size={20} />,
               label: <Link to="/payment">To'lovlar</Link>,
             },
             {
               key: '/students',
-              icon: <PiStudentFill />,
+              icon: <PiStudentFill size={20} />,
               label: <Link to="/students">O'quvchilar</Link>,
             },
             {
               key: '/settings',
-              icon: <AiFillSetting/>,
+              icon: <AiFillSetting size={20} />,
               label: <Link to="/settings">Sozlamalar</Link>,
             },
           ]}
@@ -50,10 +59,10 @@ const App = () => {
       </Sider>
 
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} className='flex items-center gap-4 justify-between'>
           <Button
             type="text"
-            icon={collapsed ? <RiMenuFold2Fill /> :  <RiMenuUnfold2Fill />}
+            icon={collapsed ? <RiMenuFold2Fill /> : <RiMenuUnfold2Fill />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: '16px',
@@ -61,13 +70,36 @@ const App = () => {
               height: 64,
             }}
           />
+
+          <Search placeholder="O'quvchilarni qidirish..." onSearch={null} enterButton className='w-1/2!' />
+
+          <div className='flex items-center gap-3'>
+            <Popconfirm
+              title="Rostdan ham chiqib ketmoqchimisiz?"
+              okText="Ha"
+              cancelText="Yo'q"
+              onConfirm={handleConfirm}
+            >
+              <Tooltip title="Chiqish" placement="bottom">
+                <Button
+                  shape="circle"
+                  icon={<CiSquareChevLeft color="white" />}
+                  danger
+                  className="!bg-red-700"
+                />
+              </Tooltip>
+            </Popconfirm>
+
+            <Tooltip title="Profil" placement="bottom">              
+              <Avatar shape='square' size="large" icon={<FaUserNurse />} className='mr-3! cursor-pointer' />
+            </Tooltip>
+          </div>
         </Header>
 
         <Content
           style={{
             margin: '24px 16px',
             padding: 24,
-            minHeight: 580,
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
           }}
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import route from './routes/route.jsx'
+import { ConfigProvider, theme } from 'antd'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={route}/>
+    <ConfigProvider 
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}>
+      <RouterProvider router={route}/>
+    </ConfigProvider>
   </StrictMode>,
 )
