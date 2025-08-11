c950ff06e4ee7ab43bd82863a3d63c06cd8a38e1
az1kkRich <az1kkrichbe@gmail.com>
2025-08-11 16:33:00 +0500

    phone bottom tabs

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   // const token = localStorage.getItem('token');
-  const token  = false
+  const token  = true
 
   return token ? <Outlet /> : <Navigate to="/login" />;
 };
