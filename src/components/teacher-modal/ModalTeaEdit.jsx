a451303544b678815acb7da58f05927f1fe4548f
Azizbek Alisherov <azizalisherov0826@gmail.com>
2025-09-02 10:56:30 +0500

    TESST

--- src/components/teacher-modal/ModalTeaEdit.jsx
+++ src/components/teacher-modal/ModalTeaEdit.jsx
@@ -1,3 +1,127 @@
+// import React, { useEffect } from "react";
+// import { Modal, Form, Input, InputNumber, Button } from "antd";
+// import useTeacher from "../../hooks/useTeacher";
+
+// const ModalTeaEdit = ({ visible, onClose, teacher }) => {
+//   const [form] = Form.useForm();
+//   const { updateTeacherMutation } = useTeacher();
+
+
+//   useEffect(() => {
+//     if (teacher) {
+//       form.setFieldsValue({
+//         username: teacher.username,
+//         name: teacher.name,
+//         email: teacher.email,
+//         phone: teacher.phone,
+//         salary: teacher.salary,
+//         password: "", // password bo‘sh boshlanishi
+//       });
+//     }
+//   }, [teacher, form]);
+
+//   const handleFinish = (values) => {
+//     const payload = {
+//       id: teacher.key,
+//       userId: teacher.key,
+//       username: values.username,
+//       name: values.name,
+//       email: values.email,
+//       phone: values.phone,
+//       salary: values.salary
+//     };
+
+//     // password faqat kiritilgan bo‘lsa yuborilsin
+//     if (values.password) {
+//       payload.user.password = values.password;
+//     }
+
+//     console.log("Update Payload:", payload);
+//     updateTeacherMutation.mutate(payload, { onSuccess: () => {onClose(), form.resetFields() } });
+//   };
+
+//   return (
+//     <Modal
+//       title="Teacher ma'lumotlarini tahrirlash"
+//       open={visible}
+//       onCancel={onClose}
+//       footer={[
+//         <Button key="cancel" onClick={onClose}>
+//           Bekor qilish
+//         </Button>,
+//         <Button key="submit" type="primary" onClick={() => form.submit()}>
+//           Saqlash
+//         </Button>,
+//       ]}
+//     >
+//       <Form form={form} layout="vertical" onFinish={handleFinish}>
+//         <Form.Item
+//           label="Username"
+//           name="username"
+//         // rules={[
+//         //   { required: true, message: "Username kiriting!" },
+//         //   { min: 6, message: "Username kamida 6 belgidan bo‘lishi kerak!" },
+//         // ]}
+//         >
+//           <Input disabled />
+//         </Form.Item>
+
+//         <Form.Item
+//           label="Yangi Parol"
+//           name="password"
+//           rules={[
+//             { min: 6, message: "Password kamida 6 belgidan bo‘lishi kerak!" },
+//           ]}
+//         >
+//           <Input.Password placeholder="Yangi parol (agar o‘zgartirilsa)" />
+//         </Form.Item>
+
+//         <Form.Item
+//           label="Ism"
+//           name="name"
+//           rules={[{ required: true, message: "Name kiriting!" }]}
+//         >
+//           <Input />
+//         </Form.Item>
+
+//         <Form.Item
+//           label="Email"
+//           name="email"
+//           rules={[{ required: true, message: "Email kiriting!" }]}
+//         >
+//           <Input />
+//         </Form.Item>
+
+//         <Form.Item
+//           label="Phone"
+//           name="phone"
+//           rules={[{ required: true, message: "Phone kiriting!" }]}
+//         >
+//           <Input />
+//         </Form.Item>
+
+//         <Form.Item
+//           label="Maosh"
+//           name="salary"
+//           rules={[{ required: true, message: "Maosh kiriting!" }]}
+//         >
+//           <InputNumber
+//             style={{ width: "100%" }}
+//             min={0}
+//             formatter={(value) =>
+//               `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+//             }
+//             parser={(value) => value.replace(/\s/g, "")}
+//           />
+//         </Form.Item>
+//       </Form>
+//     </Modal>
+//   );
+// };
+
+// export default ModalTeaEdit;
+
+
 import React, { useEffect } from "react";
 import { Modal, Form, Input, InputNumber, Button } from "antd";
 import useTeacher from "../../hooks/useTeacher";
@@ -5,7 +129,6 @@ import useTeacher from "../../hooks/useTeacher";
 const ModalTeaEdit = ({ visible, onClose, teacher }) => {
   const [form] = Form.useForm();
   const { updateTeacherMutation } = useTeacher();
-
 
   useEffect(() => {
     if (teacher) {
@@ -15,7 +138,7 @@ const ModalTeaEdit = ({ visible, onClose, teacher }) => {
         email: teacher.email,
         phone: teacher.phone,
         salary: teacher.salary,
-        password: "", // password bo‘sh boshlanishi
+        password: "", // password bo'sh boshlanishi
       });
     }
   }, [teacher, form]);
@@ -28,16 +151,22 @@ const ModalTeaEdit = ({ visible, onClose, teacher }) => {
       name: values.name,
       email: values.email,
       phone: values.phone,
-      salary: values.salary
+      salary: values.salary,
+      user: {} // user obyekti yaratish
     };
 
-    // password faqat kiritilgan bo‘lsa yuborilsin
-    if (values.password) {
+    // password faqat kiritilgan bo'lsa yuborilsin
+    if (values.password && values.password.trim()) {
       payload.user.password = values.password;
     }
 
     console.log("Update Payload:", payload);
-    updateTeacherMutation.mutate(payload, { onSuccess: () => {onClose(), form.resetFields() } });
+    updateTeacherMutation.mutate(payload, { 
+      onSuccess: () => {
+        onClose();
+        form.resetFields();
+      }
+    });
   };
 
   return (
@@ -58,10 +187,10 @@ const ModalTeaEdit = ({ visible, onClose, teacher }) => {
         <Form.Item
           label="Username"
           name="username"
-        // rules={[
-        //   { required: true, message: "Username kiriting!" },
-        //   { min: 6, message: "Username kamida 6 belgidan bo‘lishi kerak!" },
-        // ]}
+          rules={[
+            { required: true, message: "Username kiriting!" },
+            { min: 6, message: "Username kamida 6 belgidan bo'lishi kerak!" },
+          ]}
         >
           <Input disabled />
         </Form.Item>
@@ -70,10 +199,10 @@ const ModalTeaEdit = ({ visible, onClose, teacher }) => {
           label="Yangi Parol"
           name="password"
           rules={[
-            { min: 6, message: "Password kamida 6 belgidan bo‘lishi kerak!" },
+            { min: 6, message: "Password kamida 6 belgidan bo'lishi kerak!" },
           ]}
         >
-          <Input.Password placeholder="Yangi parol (agar o‘zgartirilsa)" />
+          <Input.Password placeholder="Yangi parol (agar o'zgartirilsa)" />
         </Form.Item>
 
         <Form.Item
@@ -87,7 +216,10 @@ const ModalTeaEdit = ({ visible, onClose, teacher }) => {
         <Form.Item
           label="Email"
           name="email"
-          rules={[{ required: true, message: "Email kiriting!" }]}
+          rules={[
+            { required: true, message: "Email kiriting!" },
+            { type: "email", message: "Email noto'g'ri!" },
+          ]}
         >
           <Input />
         </Form.Item>
--- src/pages/main/Teachers.jsx
+++ src/pages/main/Teachers.jsx
@@ -1,3 +1,149 @@
+// import React, { useState } from "react";
+// import { Button, Table, Space, Popconfirm, Input, Tag } from "antd";
+// import { DollarOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
+// import { Link } from "react-router-dom";
+
+// import useTeacher from "../../hooks/useTeacher";
+// import ModalTeachers from "../../components/teacher-modal/ModalTeachers";
+// import ModalTeaEdit from "../../components/teacher-modal/ModalTeaEdit";
+
+// const TeachersPage = () => {
+//     const { getTeachers, deleteTeacherMutation } = useTeacher();
+//     const [modalVisible, setModalVisible] = useState(false);
+//     const [selectedTeacher, setSelectedTeacher] = useState(null);
+//     const [searchText, setSearchText] = useState("");
+
+//     if (getTeachers.isLoading) return <p>Yuklanmoqda...</p>;
+//     if (getTeachers.isError) return <p>Error: {getTeachers.error.message}</p>;
+
+//     const teachers = getTeachers.data.map((item) => (
+//         {
+//             key: item.user.id,        // yoki id bo‘lmasa username unique bo‘lsin
+//             username: item.user.username,
+//             name: item.name,
+//             email: item.email,
+//             phone: item.phone,
+//             salary: item.salary,
+//             courses: item.courses.map((course) => course.name).join(", "), // kurs nomlarini olish va vergul bilan ajratish
+//         }
+//     ))
+    
+//     const filterTeachers = teachers.filter((teacher) =>{
+//         const search = searchText.toLowerCase();
+//         return teacher.name.toLowerCase().includes(search) 
+//     })
+
+//     const columns = [
+//         { title: "Username", dataIndex: "username", key: "username" },
+//         { title: "Ism", dataIndex: "name", key: "name" },
+//         { title: "Email", dataIndex: "email", key: "email" },
+//         { title: "Telefon", dataIndex: "phone", key: "phone" },
+//         { 
+//             title: "Kurslari", 
+//             dataIndex: "courses", 
+//             key: "courses",
+           
+//         },
+        
+//         {
+//             title: "Maosh", dataIndex: "salary", key: "salary",
+//             render: (fee) => (
+//                 <Space>
+//                     <DollarOutlined style={{ color: '#fa8c16' }} />
+//                     <Tag color="cyan">{fee?.toLocaleString()} so'm</Tag>
+//                 </Space>
+//             ),
+//         },
+//         {
+//             title: "Actions",
+//             key: "actions",
+//             render: (_, record) => (
+//                 <Space>
+//                     <Button
+//                         type="primary"
+//                         onClick={() => {
+//                             setSelectedTeacher(record);
+//                             setModalVisible(true);
+//                         }}
+//                     >
+//                         O'zgartirish
+//                     </Button>
+//                     <Popconfirm
+//                         title="Rostdan ham uchirishni holaysizmi?"
+//                         onConfirm={() => deleteTeacherMutation.mutate(record.key)}
+//                         okText="Ha"
+//                         cancelText="Yo"
+//                     >
+//                         <Button danger>O'chirish</Button>
+//                     </Popconfirm>
+//                 </Space>
+//             ),
+//         },
+//     ];
+
+//     return (
+//         <div>
+//             <div className="flex justify-between mb-4 gap-4">
+//                 <h2 className="text-xl font-bold">Ustozlar</h2>
+
+//                 <div className="flex gap-5">
+
+//                     {/* 🔎 Search input */}
+//                     <Input
+//                             placeholder="Ustozlarni qidirish... {ism}"
+//                             prefix={<SearchOutlined />}
+//                             value={searchText}
+//                             onChange={(e) => setSearchText(e.target.value)}
+//                             allowClear
+//                             style={{ width: 300, marginBottom: 16 }}
+                            
+//                         />
+//                     <Button
+//                         type="primary"
+//                         style={{ marginBottom: 16 }}
+//                         onClick={() => {
+//                             setSelectedTeacher(null); // Add rejimi uchun
+//                             setModalVisible(true);
+//                         }}
+//                         icon={<PlusOutlined />}
+//                     >
+//                         Yangi Ustoz qo'shish
+//                     </Button>
+//                 </div>
+//             </div>
+
+//             {/* Edit teacher modal */}
+//             {selectedTeacher && (
+//                 <ModalTeaEdit
+//                     visible={modalVisible}
+//                     onClose={() => {
+//                         setModalVisible(false);
+//                         setSelectedTeacher(null);
+//                     }}
+//                     teacher={selectedTeacher}
+//                 />
+//             )}
+
+//             <Table
+//                 dataSource={filterTeachers}
+//                 columns={columns}
+//                 rowKey="id"
+//             />
+
+//             <ModalTeachers
+//                 visible={modalVisible}
+//                 onClose={() => setModalVisible(false)}
+//                 teacher={selectedTeacher}
+//             />
+//         </div>
+//     );
+// };
+
+// export default TeachersPage;
+
+
+
+
 import React, { useState } from "react";
 import { Button, Table, Space, Popconfirm, Input, Tag } from "antd";
 import { DollarOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
@@ -9,29 +155,30 @@ import ModalTeaEdit from "../../components/teacher-modal/ModalTeaEdit";
 
 const TeachersPage = () => {
     const { getTeachers, deleteTeacherMutation } = useTeacher();
-    const [modalVisible, setModalVisible] = useState(false);
+    
+    // Har bir modal uchun alohida state
+    const [addModalVisible, setAddModalVisible] = useState(false);
+    const [editModalVisible, setEditModalVisible] = useState(false);
     const [selectedTeacher, setSelectedTeacher] = useState(null);
     const [searchText, setSearchText] = useState("");
 
     if (getTeachers.isLoading) return <p>Yuklanmoqda...</p>;
     if (getTeachers.isError) return <p>Error: {getTeachers.error.message}</p>;
 
-    const teachers = getTeachers.data.map((item) => (
-        {
-            key: item.user.id,        // yoki id bo‘lmasa username unique bo‘lsin
-            username: item.user.username,
-            name: item.name,
-            email: item.email,
-            phone: item.phone,
-            salary: item.salary,
-            courses: item.courses.map((course) => course.name).join(", "), // kurs nomlarini olish va vergul bilan ajratish
-        }
-    ))
+    const teachers = getTeachers.data.map((item) => ({
+        key: item.user.id,
+        username: item.user.username,
+        name: item.name,
+        email: item.email,
+        phone: item.phone,
+        salary: item.salary,
+        courses: item.courses.map((course) => course.name).join(", "),
+    }));
     
-    const filterTeachers = teachers.filter((teacher) =>{
+    const filterTeachers = teachers.filter((teacher) => {
         const search = searchText.toLowerCase();
-        return teacher.name.toLowerCase().includes(search) 
-    })
+        return teacher.name.toLowerCase().includes(search);
+    });
 
     const columns = [
         { title: "Username", dataIndex: "username", key: "username" },
@@ -42,11 +189,11 @@ const TeachersPage = () => {
             title: "Kurslari", 
             dataIndex: "courses", 
             key: "courses",
-           
         },
-        
         {
-            title: "Maosh", dataIndex: "salary", key: "salary",
+            title: "Maosh", 
+            dataIndex: "salary", 
+            key: "salary",
             render: (fee) => (
                 <Space>
                     <DollarOutlined style={{ color: '#fa8c16' }} />
@@ -63,7 +210,7 @@ const TeachersPage = () => {
                         type="primary"
                         onClick={() => {
                             setSelectedTeacher(record);
-                            setModalVisible(true);
+                            setEditModalVisible(true); // Edit modal ochish
                         }}
                     >
                         O'zgartirish
@@ -72,7 +219,7 @@ const TeachersPage = () => {
                         title="Rostdan ham uchirishni holaysizmi?"
                         onConfirm={() => deleteTeacherMutation.mutate(record.key)}
                         okText="Ha"
-                        cancelText="Yo"
+                        cancelText="Yo'q"
                     >
                         <Button danger>O'chirish</Button>
                     </Popconfirm>
@@ -87,23 +234,20 @@ const TeachersPage = () => {
                 <h2 className="text-xl font-bold">Ustozlar</h2>
 
                 <div className="flex gap-5">
-
                     {/* 🔎 Search input */}
                     <Input
-                            placeholder="Ustozlarni qidirish... {ism}"
-                            prefix={<SearchOutlined />}
-                            value={searchText}
-                            onChange={(e) => setSearchText(e.target.value)}
-                            allowClear
-                            style={{ width: 300, marginBottom: 16 }}
-                            
-                        />
+                        placeholder="Ustozlarni qidirish... {ism}"
+                        prefix={<SearchOutlined />}
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        allowClear
+                        style={{ width: 300, marginBottom: 16 }}
+                    />
                     <Button
                         type="primary"
                         style={{ marginBottom: 16 }}
                         onClick={() => {
-                            setSelectedTeacher(null); // Add rejimi uchun
-                            setModalVisible(true);
+                            setAddModalVisible(true); // Add modal ochish
                         }}
                         icon={<PlusOutlined />}
                     >
@@ -112,28 +256,26 @@ const TeachersPage = () => {
                 </div>
             </div>
 
+            {/* Add teacher modal */}
+            <ModalTeachers
+                visible={addModalVisible}
+                onClose={() => setAddModalVisible(false)}
+            />
+
             {/* Edit teacher modal */}
-            {selectedTeacher && (
-                <ModalTeaEdit
-                    visible={modalVisible}
-                    onClose={() => {
-                        setModalVisible(false);
-                        setSelectedTeacher(null);
-                    }}
-                    teacher={selectedTeacher}
-                />
-            )}
+            <ModalTeaEdit
+                visible={editModalVisible}
+                onClose={() => {
+                    setEditModalVisible(false);
+                    setSelectedTeacher(null);
+                }}
+                teacher={selectedTeacher}
+            />
 
             <Table
                 dataSource={filterTeachers}
                 columns={columns}
-                rowKey="id"
-            />
-
-            <ModalTeachers
-                visible={modalVisible}
-                onClose={() => setModalVisible(false)}
-                teacher={selectedTeacher}
+                rowKey="key" // "id" emas, "key" bo'lishi kerak
             />
         </div>
     );
